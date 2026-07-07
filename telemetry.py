d73a6dbaf975068ecc5cb89f7325db6d41c9cc39
Argiris Aitos <aitos.ar49@gmail.com>
2026-07-07 20:19:59 +0300

    Process and publish telemetry data
    Added continuous MAVLink telemetry acquisition, data conversion and real-time publishing of GPS, battery, altitude, speed and vertical velocity through MQTT topics

--- telemetry.py
+++ telemetry.py
@@ -42,3 +42,46 @@ client.connect(MQTT_HOST, MQTT_PORT, 60) # Connects to the local IP address
 client.loop_start() # Starts running the MQTT in the background
 print("MQTT Connected!")
 
+last_gps = 0 # Initialize timestamp for the last global posistion
+last_bat = 0 # Initialize timestamp for the last battery
+last_speed = 0 #
+
+print("Starting Main Loop... Waiting for messages.")
+
+while True: 
+    msg = m.recv_match(blocking=False) # Read incoming data 
+    if not msg:
+        time.sleep(0.01)
+        continue # Reset of the loop
+
+    t = time.time()
+    msg_type = msg.get_type() # Read the message name
+    # Battery (Volt) 
+    if msg_type == "SYS_STATUS" and (t - last_bat) > 1.0: 
+        voltage = msg.voltage_battery / 1000.0 # Convert battery voltage from millivolts to volts 
+        print(f"Battery Voltage: {voltage:.2f} V")
+        client.publish(TOPIC_BAT, str(voltage)) # Send voltage to broker
+        last_bat = t # Update the battery timestamp
+
+    # GPS & SPEED
+    if msg_type == "GLOBAL_POSITION_INT" and (t - last_gps) > 0.2:
+        lat = msg.lat / 1e7 # Convert latitude to decimal degrees
+        lon = msg.lon / 1e7 # Convert longitude to decimal degrees
+        alt = msg.relative_alt / 1000.0  # Relative altitude (m) 
+        print(f"GPS: Lat={lat}, Lon={lon}, Alt={alt}m")
+        gps_payload = {"lat": lat, "lon": lon, "alt": alt} # Create a JSON with the GPS data
+        client.publish(TOPIC_GPS, json.dumps(gps_payload)) # Publish GPS data as JSON
+        
+        # Ground speed
+        vx = msg.vx / 100.0 # Convert X axis velocity from cm/s to m/s
+        vy = msg.vy / 100.0 # Convert Y axis velocity from cm/s to m/s
+        speed = (vx*vx + vy*vy) ** 0.5 # Calculate total ground speed
+        client.publish(TOPIC_SPEED, str(speed)) # Convert calculated speed to string and publish via MQTT
+        # Vertical
+        vz= -(msg.vz / 100.0) # Convert Z axis velocity to m/s  
+        client.publish(TOPIC_VERTICAL, str(vz)) # Convert vertical speed to string and publish via MQTT
+        
+        # alt
+        alt_m= msg.relative_alt / 1000.0 # Convert relative altitude from millimeters to meters
+        client.publish(TOPIC_ALT, str(alt_m)) # Convert altitude to string and publish via MQTT
+        last_gps = t # Update the last global timestamp
