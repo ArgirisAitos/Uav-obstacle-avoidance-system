981b19829e3546ecffcfd8687ee607e6bb362c9d
Argiris Aitos <aitos.ar49@gmail.com>
2026-07-07 18:49:30 +0300

    Connection to mqtt

--- telemetry.py
+++ telemetry.py
@@ -25,3 +25,20 @@ try:
 except Exception as e:
     print(f"Error connecting to Pixhawk: {e}")
     exit(1)
+
+m.mav.request_data_stream_send(
+    m.target_system,
+    m.target_component,
+    mavutil.mavlink.MAV_DATA_STREAM_ALL,
+    4,  # Rate (Hz) 
+    1   # Start
+)
+print("Data Stream Requested (All streams enabled)")
+
+# MQTT 
+print("Connecting to MQTT Broker...")
+client = mqtt.Client() # Creates the mechanism that handles the communication
+client.connect(MQTT_HOST, MQTT_PORT, 60) # Connects to the local IP address
+client.loop_start() # Starts running the MQTT in the background
+print("MQTT Connected!")
+
